048b203ec1b451a9397e71dc35f705f5a2f6523a
TheHellTower <thehelltower@tuta.io>
2023-09-10 14:03:43 -0400

    Runtime

--- WeirdStrings/Runtime.cs
+++ WeirdStrings/Runtime.cs
@@ -10,7 +10,7 @@ namespace WeirdStrings
         //X = Pos Start & Y = Pos End so Start of string and End of string.
         internal static string TheHellTower(this int x, int y)
         {
-            if(Assembly.GetCallingAssembly() == Assembly.GetExecutingAssembly())
+            if (Assembly.GetCallingAssembly() == Assembly.GetExecutingAssembly())
             {
                 //StackFrame 1 = Caller Method/Method that called this method.
                 var z = new StackFrame(1).GetMethod().Name;
@@ -20,7 +20,7 @@ namespace WeirdStrings
 
                 return Encoding.UTF8.GetString(Convert.FromBase64String(z.Substring(x, y - x).Replace("*", "="))); //here we are only taking from start to end to get the right string.
             }
-            return string.Join("", new string[] { "T", "h","e","H","e","l","l","T","o","w","e","r",","," ","b","a","s","i","c"," ","A","n","t","i","-","I","n","v","o","k","e"," ","s","o"," ","y","o","u"," ","s","h","o","u","l","d"," ","b","e"," ","o","k","a","y"," ","!"," ",":","'",")","" });
+            return string.Join("", new string[] { "T", "h", "e", "H", "e", "l", "l", "T", "o", "w", "e", "r", ",", " ", "B", "a", "s", "i", "c", " ", "A", "n", "t", "i", "-", "I", "n", "v", "o", "k", "e", " ", "s", "o", " ", "y", "o", "u", " ", "s", "h", "o", "u", "l", "d", " ", "b", "e", " ", "o", "k", "a", "y", " ", "!", " ", ":", "'", ")" });
         }
     }
 }
